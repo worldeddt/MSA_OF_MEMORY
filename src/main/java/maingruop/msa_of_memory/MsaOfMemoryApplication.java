f754593dd5499889e9dea9230a6eb6184ebdb560
김태형 <ktest92@gmail.com>
2023-04-13 08:37:35 +0900

    user controller

--- src/main/java/maingruop/msa_of_memory/MsaOfMemoryApplication.java
+++ src/main/java/maingruop/msa_of_memory/MsaOfMemoryApplication.java
@@ -3,10 +3,16 @@ package maingruop.msa_of_memory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class MsaOfMemoryApplication {
 
     public static void main(String[] args) {
+        List<String> df = new ArrayList<>();
+
+        df.size();
         SpringApplication.run(MsaOfMemoryApplication.class, args);
     }
 
