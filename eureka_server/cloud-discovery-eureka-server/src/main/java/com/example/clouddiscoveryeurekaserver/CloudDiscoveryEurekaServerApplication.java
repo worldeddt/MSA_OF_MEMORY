8adc7f036ef5ef5617ddc94eb811c64d49ccbe3c
김태형 <ktest92@gmail.com>
2023-07-04 19:14:40 +0900

    eureka 설정 대비 api-gate-way 대응

--- eureka_server/cloud-discovery-eureka-server/src/main/java/com/example/clouddiscoveryeurekaserver/CloudDiscoveryEurekaServerApplication.java
+++ eureka_server/cloud-discovery-eureka-server/src/main/java/com/example/clouddiscoveryeurekaserver/CloudDiscoveryEurekaServerApplication.java
@@ -11,5 +11,4 @@ public class CloudDiscoveryEurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudDiscoveryEurekaServerApplication.class, args);
     }
-
 }
