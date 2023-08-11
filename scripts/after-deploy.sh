bf868977bbda6c7ea39d0a1c38dde1bf7e005374
김태형 <ktest92@gmail.com>
2023-08-11 09:02:38 +0900

    git actions 설정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/partners
+REPOSITORY=/home/ubuntu
 
-cd $REPOSITORY
+cd $REPOSITORY/api-gateway/apigate-service
 
-#sudo /usr/bin/yarn
-#sudo /usr/bin/yarn db:pull //prisma 사용했을 경우만
-#sudo /usr/bin/yarn generate //prisma 사용했을 경우만 - 해당명령어는 위의 scripts의 명령어임
-#sudo /usr/bin/pm2 start dist
-sudo mv dist/* /var/www/html/partners/
-sudo rm -rf dist
+sudo ./mvnw clean package -DskipTests
 
-echo "upload partners complete"
+echo "upload complete"
