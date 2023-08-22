b71802cd604342746cd7a50c5b9d2e7d401931cf
김태형 <ktest92@gmail.com>
2023-08-22 09:12:20 +0900

    eureka 설정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu
+REPOSITORY=/var/www/html
 
 cd $REPOSITORY/api-gateway/apigate-service
 
 echo $(sudo pwd)
+sudo ./mvnw clean package -DskipTests &&
+cd $REPOSITORY/eureka_server/cloud-discovery-eureka-server &&
 sudo ./mvnw clean package -DskipTests
 
 echo "upload complete"
