73af702b26276f41570f564c299ba3e504d8cab3
김태형 <ktest92@gmail.com>
2023-09-01 08:35:01 +0900

    JAVA_HOME 설정 안되어 있을 경우 대비

--- springKafka/entrypoint.sh
+++ springKafka/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 echo "build start";
-echo $(./mvnw clean package -DskipTests)
-echo $(mv target/*.jar spring-kafka.jar)
-echo $(chmod 744 spring-kafka.jar)
-echo $(nohup java -jar $(pwd)/spring-kafka.jar &)
+
+javahome = $(echo $JAVA_HOME)
+
+# shellcheck disable=SC1073
+# shellcheck disable=SC1019
+# shellcheck disable=SC1072
+# shellcheck disable=SC1020
+if [ -z javahome]; then
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+fi
+
+#echo $(./mvnw clean package -DskipTests)
+#echo $(mv target/*.jar spring-kafka.jar)
+#echo $(chmod 744 spring-kafka.jar)
+#echo $(nohup java -jar $(pwd)/spring-kafka.jar &)
 echo "build end";
--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 echo "build start";
-echo $(./mvnw clean package -DskipTests)
-echo $(mv target/*.jar user-service.jar)
-echo $(chmod 744 user-service.jar)
-echo $(nohup java -jar $(pwd)/user-service.jar &)
+
+javahome = $(echo $JAVA_HOME)
+
+# shellcheck disable=SC1073
+# shellcheck disable=SC1019
+# shellcheck disable=SC1072
+# shellcheck disable=SC1020
+if [ -z javahome]; then
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+fi
+
+#echo $(./mvnw clean package -DskipTests)
+#echo $(mv target/*.jar user-service.jar)
+#echo $(chmod 744 user-service.jar)
+#echo $(nohup java -jar $(pwd)/user-service.jar &)
 echo "build end";
